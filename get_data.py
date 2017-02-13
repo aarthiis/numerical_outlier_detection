b340a6de1f943f419158b769429f017d93ed2ad6
yashwantkeswani <yashwant.keswani@gmail.com>
2017-02-14 03:01:39 +0530

    Add function get_types

--- get_data.py
+++ get_data.py
@@ -2,6 +2,30 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 
 
+def get_types(entity_URI):
+    query ="""
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX : <http://dbpedia.org/resource/>
+    PREFIX dbpedia2: <http://dbpedia.org/property/>
+    PREFIX dbpedia: <http://dbpedia.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX yago:<http://dbpedia.org/class/yago/>
+
+    SELECT DISTINCT ?concept
+    WHERE {
+        %s a ?concept .
+    }
+    """ % (entity_URI)
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
+    sparql.setQuery(query)  # the previous query as a literal string
+    return sparql.query().convert()
 
 def get_country_population():
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
