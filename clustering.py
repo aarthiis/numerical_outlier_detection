7cc1154ba32fe1f4152a01212af7bdff0b547edf
yashwantkeswani <yashwant.keswani@gmail.com>
2017-02-15 03:02:41 +0530

    Clustering Module complete

--- clustering.py
+++ clustering.py
@@ -1,6 +1,7 @@
 
 import get_data as gd
 import numpy as np
+from sklearn import mixture
 
 def get_all_types(ordered_entities):
     """This function will get all the types related with a URI.
@@ -18,7 +19,7 @@ def get_all_types(ordered_entities):
     entity_type = {}
     for each in ordered_entities:
         entity_type_each_list = []
-        results = gd.get_types(":" + each.split("/")[-1])['results']['bindings']
+        results = gd.get_types("<" + each + ">")['results']['bindings']
         for each_type in results:
             _type = each_type['concept']['value']
             if _type not in all_types:
@@ -59,11 +60,46 @@ def feature_index_map(all_types, discards):
     
 def prepare_dataset(ordered_entities, p = 0.05):
     entity_type, all_types = get_all_types(ordered_entities)
-    discards = discard_p(all_types, p)
+    discards = discard_p(all_types, len(all_types), p)
     feature_map = feature_index_map(all_types, discards)
     data_feature = np.zeros(shape = (len(ordered_entities), len(feature_map)), dtype = int)
+    print(feature_map)
     for i in range(len(ordered_entities)):
         for j in entity_type[ordered_entities[i]]:
-            data_feature[i][feature_map[j]] = 1
+            try:
+                data_feature[i][feature_map[j]] = 1
+            except Exception as e:
+                pass
+                #print("Feature Removed Because of p constraints")
     return data_feature
     
+def find_best_gmm(X,n = 7):
+    """This function attempt to find the best GMM for a 
+    given dataset. It tries to find the best no of clusters
+    and the different cv_types
+    The arguments are:
+    1. X which is the dataset
+    2. n which is the number of clusters upto which it needs to check"""
+    
+    lowest_bic = np.infty
+    bic = []
+    n_components_range = range(1, n)
+    cv_types = ['spherical', 'tied', 'diag', 'full']
+    for cv_type in cv_types:
+        for n_components in n_components_range:
+            # Fit a Gaussian mixture with EM
+            gmm = mixture.GaussianMixture(n_components=n_components,
+                                          covariance_type=cv_type)
+            gmm.fit(X)
+            bic.append(gmm.bic(X))
+            if bic[-1] < lowest_bic:
+                lowest_bic = bic[-1]
+                best_gmm = gmm
+    return best_gmm
+
+
+def cluster(ordered_entities, p =0.05):
+    """This is the final function which is used to cluster"""
+    ds = prepare_dataset(ordered_entities, p = p)
+    best_gmm = find_best_gmm(ds, 10)
+    return best_gmm.predict(ds)
