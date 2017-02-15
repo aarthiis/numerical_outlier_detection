d36af797a93e22229d2b1c5206233aa20c706127
yashwantkeswani <yashwant.keswani@gmail.com>
2017-02-15 16:40:01 +0530

    Incorporate Clustering Module

--- main.py
+++ main.py
@@ -8,12 +8,38 @@ def iqr(data, upper, lower):
     qUpper, qLower = np.percentile(data, [upper,lower])
     return qUpper - qLower
 
-def find_outliers_using_MAD(data, multiplyingFactor=1):
-    median = np.median(data)
-    MAD = np.median(np.abs(data - median))
-    return np.where(np.abs(data-median)>=(multiplyingFactor*MAD))
+def find_outliers_using_MAD(data, multiplyingFactor=1, preprocess = False, clusters = None):
+    """This function is used to find outliers using the Median Absolute Dispersion
     
-def find_outliers_using_IQR(data, upper = 75, lower = 25, multiplyingFactor = 1.5):
+        The arguments are
+        data: The numpy array in which we want to find the outliers.        
+        multiplyingFactor : This is the multiplying factor        
+        preprocess : This is a boolean which sets the \
+                                        options to enable preprocessing
+        clusters : The clusters which are provided if preprocess \
+                                            is set to true (numpy array)
+    
+        The function returns a numpy array which contains the indices of the numpy \
+                    array, which are outliers.
+
+    """
+    if preprocess and clusters is not None:
+        no_of_clusters = np.maximum(clusters) + 1
+        indices = np.empty(shape=(0,1))
+        for i in range(no_of_clusters):
+            data_cluster_i = data[clusters==i]
+            median = np.median(data_cluster_i)
+            MAD = np.median(np.abs(data - median))
+            indices = np.concatenate((np.where(np.abs(data_cluster_i-median)>=\
+                                        (multiplyingFactor*MAD)), indices), axis=0)
+        return indices
+    else:
+        median = np.median(data)
+        MAD = np.median(np.abs(data - median))
+        return np.where(np.abs(data-median)>=(multiplyingFactor*MAD))
+        
+def find_outliers_using_IQR(data, upper = 75, lower = 25, multiplyingFactor = 1.5, \
+                                              preprocess = False, clusters = None):
     """The function is used to find outliers from a given numpy array.
 
 
@@ -22,15 +48,31 @@ def find_outliers_using_IQR(data, upper = 75, lower = 25, multiplyingFactor = 1.
         upper: The higher percentile value to calculate the IQR
         lower: The lower percentile value to calculate the IQR
         multiplyingFactor: This is the multiplying factor for the IQR
-
-        The function returns a numpy array which contains the indices of the numpy array, \
-                            which are outliers.
+        preprocess : This is a boolean which sets the \
+                                        options to enable preprocessing
+        clusters : The clusters which are provided if preprocess \
+                                            is set to true (numpy array)
+        The function returns a numpy array which contains the indices of the numpy \
+                    array, which are outliers.
     """
     if lower>upper:
         lower, upper = upper, lower
-    qUpper, qLower = np.percentile(data, [upper,lower])
-    iqrFactored = multiplyingFactor*(qUpper-qLower)
-    return np.where((data<(qLower-iqrFactored)) | (data>(qUpper+iqrFactored)))
+
+
+    if preprocess and clusters is not None:
+        no_of_clusters = np.maximum(clusters) + 1
+        indices = np.empty(shape=(0,1))
+        for i in range(no_of_clusters):
+            data_cluster_i = data[clusters==i]
+            qUpper, qLower = np.percentile(data_cluster_i, [upper,lower])
+            iqrFactored = multiplyingFactor*(qUpper-qLower)
+            indices = np.concatenate((np.where((data_cluster_i<(qLower-iqrFactored)) | \
+                        (data_cluster_i>(qUpper+iqrFactored))), indices), axis = 0)
+            return indices
+    else:
+        qUpper, qLower = np.percentile(data, [upper,lower])
+        iqrFactored = multiplyingFactor*(qUpper-qLower)
+        return np.where((data<(qLower-iqrFactored)) | (data>(qUpper+iqrFactored)))
     
 def statistics(data):
     """This function prints the following statistics
@@ -43,23 +85,44 @@ def statistics(data):
 def gauss(x, mu, sigma):
     return (1/sigma*(np.sqrt(2*np.pi)))*np.exp((-1/(2*sigma*sigma))*((x-mu)**2))
 
-def KDE(x, h = None, threshold = 1):
+def KDE(x, h = None, threshold = 1, preprocess = False, clusters = None):
     """The function is used to find the outliers using
     the Kernel Density Estimation Technique"""
-    n = x.size
-    fnh = np.zeros(n)
-    mu = np.mean(x)
-    sigma = np.std(x)
     if h is None:
         h = (((4*(sigma**5))/(3*n))**0.2)
 
-   
-    for i in range(x.size):
-        fnh[i] = np.sum(gauss((x-x[i])/h, mu, sigma))/(n*h)
-    
-    normalizing = np.sum(fnh)/n
-    fnh = fnh/normalizing
-    return np.where(fnh<threshold)
+    if preprocess and clusters is not None:
+        indices = np.empty(shape=(0,1))
+        no_of_clusters = np.maximum(clusters) + 1
+        for i in range(no_of_clusters):
+            data_cluster_i = x[clusters==i]
+            n = data_cluster_i.size
+            fnh = np.zeros(n)
+            mu = np.mean(data_cluster_i)
+            sigma = np.std(data_cluster_i)
+
+           
+            for i in range(data_cluster_i.size):
+                fnh[i] = np.sum(gauss((data_cluster_i-data_cluster_i[i])/h, \
+                                mu, sigma))/(n*h)
+            
+            normalizing = np.sum(fnh)/n
+            fnh = fnh/normalizing
+            indices = np.concatenate((indices, np.where(fnh<threshold)), axis = 0)
+        return indices
+    else:
+        n = x.size
+        fnh = np.zeros(n)
+        mu = np.mean(x)
+        sigma = np.std(x)
+
+       
+        for i in range(x.size):
+            fnh[i] = np.sum(gauss((x-x[i])/h, mu, sigma))/(n*h)
+        
+        normalizing = np.sum(fnh)/n
+        fnh = fnh/normalizing
+        return np.where(fnh<threshold)
     
       
 
